983b09a3b92797de37d6172fb803b7a02261623f
Kuba <ksulaymanov@gmail.com>
2022-07-09 22:16:29 -0500

    added Product model and admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from store.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'prod_name', 'prod_category', 'balance', 'price']
+    list_display_links = ['prod_name']
+    list_filter = ['balance']
+    search_fields = ['prod_name', 'prod_category']
+    fields = ['prod_name', 'description', 'prod_category', 'balance', 'price', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(Product, ProductAdmin)
