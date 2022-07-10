a2a66486b984bdf50cdcbba235d3c7776fc90a6a
Kuba <ksulaymanov@gmail.com>
2022-07-10 01:52:24 -0500

    fixed typo in fixtures, added order by name and category. added filter to not show product if balance is 0 on the home page

--- djangoProject48/urls.py
+++ djangoProject48/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from store.views import index_view
+from store.views import index_view, product_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index_view, name='index'),
+    path('product/<int:pk>', product_view, name="product_view")
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.core.validators import MinValueValidator
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from store.models import Product
 
 
 def index_view(request):
-    product = Product.objects.order_by('-created_at')
+    product = Product.objects.order_by('prod_category', 'prod_name')
     return render(request, 'index.html', {"product": product})
+
+
+def product_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product.html', {"product": product})
